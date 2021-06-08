3dfef77be91769b7b13d48e7f82fb362e998fd06
masum <mail@domain.com>
2021-06-08 23:04:56 +0600

    first commit

---
+++ config.csrf-secret.php
@@ -0,0 +1,1 @@
+<?php $secret = "817d7fcdf3154b1987803b73c53ab08ed5bc340b";
---
+++ user_privileges/user_privileges_1.php
@@ -0,0 +1,9 @@
+<?php
+
+
+//This is the access privilege file
+$is_admin=true;
+
+$user_info=array('user_name'=>'admin','is_admin'=>'on','user_password'=>'$2y$10$vTlxy9c8ENa./jSjL8YT0.D391FPAv9K9b9FP9fCCoOZf9EFIc3Ue','confirm_password'=>'$2y$10$AbKvRtXYPET8bz5OM7016.gV.Ue7ROcfCm99PjRyevQ1e4E8Ouyya','first_name'=>'shupanto','last_name'=>'Administrator','roleid'=>'H2','email1'=>'linuxplaner@gmail.com','status'=>'Active','activity_view'=>'This Week','lead_view'=>'Today','hour_format'=>'12','end_hour'=>'23:00','start_hour'=>'00:00','is_owner'=>'1','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'mm-dd-yyyy','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'GpED7ajfDdaWZHUY','time_zone'=>'Asia/Dhaka','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'1','theme'=>'alphagrey','language'=>'en_us','reminder_interval'=>'1 Minute','currency_name'=>'USA, Dollars','currency_code'=>'USD','currency_symbol'=>'&#36;','conv_rate'=>'1.00000','phone_crm_extension'=>'','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'1','dayoftheweek'=>'Sunday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'','default_record_view'=>'Summary','leftpanelhide'=>'0','rowheight'=>'','defaulteventstatus'=>'Planned','defaultactivitytype'=>'Call','hidecompletedevents'=>'','defaultcalendarview'=>'MyCalendar','defaultlandingpage'=>'Home','record_id'=>'','record_module'=>'','id'=>'1');
+
+?>
